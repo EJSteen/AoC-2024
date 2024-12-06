bbf77a0236743bc67d55a22e8cc1531e9ce8c443
Erjan Steenbergen <e.j.steenbergen@student.utwente.nl>
2024-12-07 00:18:06 +0100

    Day6 part2

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,5 +1,7 @@
+import copy
+
 def parse_input():
-    with open('input.txt') as f:
+    with open('tests.txt') as f:
         lines = f.readlines()
     matrix = [list(line.strip()) for line in lines]
     return matrix
@@ -13,14 +15,13 @@ def find_guard(matrix):
     
     # If no guard is found, raise an error
     raise ValueError("None of the guards ('^', 'v', '<', '>') are in the list")
-
-#def process_step(matrix, index):
     
     
 def part1():
     matrix = parse_input()
     index = find_guard(matrix)
     counter = 0
+    xs = []
     
     while True:
         index = find_guard(matrix)
@@ -29,6 +30,7 @@ def part1():
                 is_out_of_bounds = index[1]-1 < 0
                 if is_out_of_bounds:
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
                     break
                 is_obstacle = matrix[index[0]][index[1]-1] == '#'
                 if is_obstacle:
@@ -36,10 +38,12 @@ def part1():
                 else:
                     matrix[index[0]][index[1]-1] = '<'
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
             case '^':
                 is_out_of_bounds = index[0]-1 < 0
                 if is_out_of_bounds:
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
                     break
                 is_obstacle = matrix[index[0]-1][index[1]] == '#'
                 if is_obstacle:
@@ -47,10 +51,12 @@ def part1():
                 else:
                     matrix[index[0]-1][index[1]] = '^'
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
             case '>':
                 is_out_of_bounds = index[1]+1 >= len(matrix[0])
                 if is_out_of_bounds:
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
                     break
                 is_obstacle = matrix[index[0]][index[1]+1] == '#'
                 if is_obstacle:
@@ -58,10 +64,12 @@ def part1():
                 else:
                     matrix[index[0]][index[1]+1] = '>'
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
             case 'v':
                 is_out_of_bounds = index[0]+1 >= len(matrix)
                 if is_out_of_bounds:
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
                     break
                 is_obstacle = matrix[index[0]+1][index[1]] == '#'
                 if is_obstacle:
@@ -69,17 +77,102 @@ def part1():
                 else:
                     matrix[index[0]+1][index[1]] = 'v'
                     matrix[index[0]][index[1]] = 'X'
+                    xs.append((index[0], index[1]))
     for line in matrix:
         counter += line.count('X')
+    return counter, xs
+        
+def part2():
+    amount_xs, xs = part1()
+    print(xs)
+
+    matrix = parse_input()
+    index = find_guard(matrix)
+    counter = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(i, j)
+            testmatrix = copy.deepcopy(matrix)
+            if not index[0] == i and not index[1] == j:
+                testmatrix[i][j] = '#'
+            if walk_patrol(testmatrix, index):
+                counter += 1
+                print(counter)
     return counter
         
-
-            
-
+def walk_patrol(matrix, index):
+    is_visited = {}
+    while True:
+        index = find_guard(matrix)
+        coords = (index[0], index[1])
+        print(index, is_visited)
+        match index[2]:
+            case '<':
+                is_out_of_bounds = index[1]-1 < 0
+                if is_out_of_bounds:
+                    break
+                if coords not in is_visited:
+                    is_visited[coords] = '<'
+                elif is_visited[coords] == '<':
+                    return True
+                is_visited[coords] = '<'
+                is_obstacle = matrix[index[0]][index[1]-1] == '#'
+                if is_obstacle:
+                    matrix[index[0]][index[1]] = '^'
+                else:
+                    matrix[index[0]][index[1]-1] = '<'
+                    matrix[index[0]][index[1]] = '.'
+            case '^':
+                is_out_of_bounds = index[0]-1 < 0
+                if is_out_of_bounds:
+                    break
+                if coords not in is_visited:
+                    is_visited[coords] = '^'
+                elif is_visited[coords] == '^':
+                    return True
+                is_visited[coords] = '^'
+                is_obstacle = matrix[index[0]-1][index[1]] == '#'
+                if is_obstacle:
+                    matrix[index[0]][index[1]] = '>'
+                else:
+                    matrix[index[0]-1][index[1]] = '^'
+                    matrix[index[0]][index[1]] = '.'
+            case '>':
+                is_out_of_bounds = index[1]+1 >= len(matrix[0])
+                if is_out_of_bounds:
+                    break
+                if coords not in is_visited:
+                    is_visited[coords] = '>'
+                elif is_visited[coords] == '>':
+                    return True
+                is_visited[coords] = '>'
+                is_obstacle = matrix[index[0]][index[1]+1] == '#'
+                if is_obstacle:
+                    matrix[index[0]][index[1]] = 'v'
+                else:
+                    matrix[index[0]][index[1]+1] = '>'
+                    matrix[index[0]][index[1]] = '.'
+            case 'v':
+                is_out_of_bounds = index[0]+1 >= len(matrix)
+                if is_out_of_bounds:
+                    break
+                if coords not in is_visited:
+                    is_visited[coords] = 'v'
+                elif is_visited[coords] == 'v':
+                    return True
+                is_visited[coords] = 'v'
+                is_obstacle = matrix[index[0]+1][index[1]] == '#'
+                if is_obstacle:
+                    matrix[index[0]][index[1]] = '<'
+                else:
+                    matrix[index[0]+1][index[1]] = 'v'
+                    matrix[index[0]][index[1]] = '.'
+    return False
+         
     
-
 def get_answer():
-    print(part1())
+    print(part2())
 
 if __name__ == "__main__":
     get_answer()
