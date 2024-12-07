8600ab70c8bac572f757b06dd0cbadcf1567aac2
Erjan Steenbergen <e.j.steenbergen@student.utwente.nl>
2024-12-07 12:47:08 +0100

    Day7 part2

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -15,24 +15,34 @@ def part1(puzzle):
         counter += try_equation(equation, ops)
     return counter
 
+def part2(puzzle):
+    counter = 0
+    ops = ['+', '*', '||']
+    for equation in puzzle:
+        counter += try_equation(equation, ops)
+    return counter
+
 def try_equation(equation, ops):
+    sum = equation[0]
     num_ops = len(equation)-2
     op_combinations = itertools.product(ops, repeat=num_ops)
     for combination in op_combinations:
         result = equation[1]
-
         for i, op, in enumerate(combination):
             if op == '+':
                 result += equation[i+2]
-            else:
+            elif op == '*':
                 result *= equation[i+2]
-        if result == equation[0]:
-            return equation[0]
+            else:
+                result = int(str(result)+str(equation[i+2]))
+        if result == sum:
+            return sum
     return 0
 
 def solve():
     puzzle = parse_input()
     print(part1(puzzle))
+    print(part2(puzzle))
 
 
 def get_answer():
