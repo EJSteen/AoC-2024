1601f353fc1f484fb2e0f89c825b8361832039ab
Erjan Steenbergen <e.j.steenbergen@student.utwente.nl>
2024-12-10 12:59:04 +0100

    Day10 part2

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,12 +1,22 @@
 from time import time
 
 def parse_input():
-    with open('test.txt') as f:
+    with open('input.txt') as f:
         lines = f.readlines()
         matrix = [[int(x) for x in list(line.strip())] for line in lines]
     return matrix
 
-def part1(matrix):
+def part1and2(matrix):
+    counter1 = 0
+    counter2 = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 0:
+                counter1 += walk(matrix, i, j, [])
+                counter2 += rate(matrix,i, j, [])
+    return counter1, counter2
+
+def part2(matrix):
     counter = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
@@ -14,28 +24,29 @@ def part1(matrix):
                 counter += walk(matrix, i, j, [])
     return counter
 
-
-
-def is_trailhead(matrix, row, col):
+def rate(matrix, row, col, visited):
     trails = 0
+    outgoing = 0
+    if matrix[row][col] == 9:
+        return 1           
     if row+1 < len(matrix) and matrix[row][col] + 1 == matrix[row+1][col]:
-        trails += 1
-    if row-1 > 0 and matrix[row][col] + 1 == matrix[row-1][col]:
-        trails += 1
+        outgoing += 1
+        trails += rate(matrix, row+1, col, visited)
+    if row-1 >= 0 and matrix[row][col] + 1 == matrix[row-1][col]:
+        outgoing += 1
+        trails += rate(matrix, row-1, col, visited)
     if col+1 < len(matrix[0]) and matrix[row][col] + 1 == matrix[row][col+1]:
-        trails += 1
-    if col-1 > 0 and matrix[row][col] + 1 == matrix[row][col-1]:
-        trails += 1
-    if trails > 1:
-        return True
-    else:
-        return False
+        outgoing += 1
+        trails += rate(matrix, row, col+1, visited)
+    if col-1 >= 0 and matrix[row][col] + 1 == matrix[row][col-1]:
+        outgoing += 1
+        trails += rate(matrix, row, col-1, visited)
+    return trails
 
 def walk(matrix, row, col, visited):
     if (row,col) in visited:
         return 0
     visited.append((row, col))
-    #print("height: ",matrix[row][col], row, col)
     counter = 0
     if matrix[row][col] == 9:
         return 1           
@@ -53,9 +64,7 @@ def walk(matrix, row, col, visited):
 
 def solve():
     puzzle = parse_input()
-    print(puzzle)
-    print("Part 1:",part1(puzzle))
-    #print("Part 2:",part2(fs))
+    print("Part 1 and 2:",part1and2(puzzle))
 
 def get_answer():
     start_time = time()
